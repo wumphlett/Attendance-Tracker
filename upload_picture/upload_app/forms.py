74d0d44790cee97efb908f4c06b46812694114dd
Qingtao Lu <61167546+qwecdkey@users.noreply.github.com>
2023-03-18 09:52:31 -0400

    Delete upload_picture directory

--- upload_picture/upload_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Picture
-
-class pic_Form(forms.ModelForm):
-    class Meta:
-        model = Picture
-        fields = ['picture']      
--- upload_picture/upload_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Picture(models.Model):
-    picture = models.ImageField(upload_to='pictures/')
