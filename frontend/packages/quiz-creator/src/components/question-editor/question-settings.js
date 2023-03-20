67f2cec3484780195507ca3b81bc8c83cdb1f550
Ethan Brown <ewbrowntech@gmail.com>
2023-03-20 14:18:06 -0700

    Added handleCheckboxChange function

--- frontend/packages/quiz-creator/src/components/question-editor/question-settings.js
+++ frontend/packages/quiz-creator/src/components/question-editor/question-settings.js
@@ -10,4 +10,36 @@
 import React from "react";
 // Components
 import { Checkbox } from "@frontend/common/build"
+// Functions
+import handleCheckboxChange from "../../functions/handleCheckboxChange";
+// Stylesheets
+import "bootstrap/dist/css/bootstrap.css"
+import "../../stylesheets/question-options.css"
+
+class QuestionSettings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = props.state;
+        this.setQuestions = props.setQuestions
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.state.activeQuestion !== this.props.state.activeQuestion) {
+            this.setState({ activeQuestion: this.props.state.activeQuestion,
+                questions: this.props.state.questions })
+        }
+        if (prevProps.state.questions !== this.props.state.questions) {
+            this.setState({ activeQuestion: this.props.state.activeQuestion,
+                questions: this.props.state.questions })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
 
---
+++ frontend/packages/quiz-creator/src/functions/handleCheckboxChange.js
@@ -0,0 +1,34 @@
+/**
+ * handleCheckboxChange.js
+ *
+ * @Author - Ethan Brown - ewbrowntech@gmail.com
+ * @Version - 20 MAR 23
+ *
+ * When a checkbox is toggled in question settings, change the respective property of the active question
+ */
+// Functions
+import modifyQuestion from './modifyQuestion'
+
+function handleCheckboxChange(label, checkboxState, activeQuestion, questions, setQuestions) {
+    if (label === "Enforce time limit") {
+        let modifiedQuestion = activeQuestion
+        modifiedQuestion.isTimeLimited = checkboxState
+        modifyQuestion(modifiedQuestion, questions, setQuestions)
+    }
+    else if (label === "Allow partial credit") {
+        let modifiedQuestion = activeQuestion
+        modifiedQuestion.isPartialCreditAllowed = checkboxState
+        modifyQuestion(modifiedQuestion, questions, setQuestions)
+    }
+    else if (label === "Allow multiple selection") {
+        let modifiedQuestion = activeQuestion
+        modifiedQuestion.isMultipleSelectionAllowed = checkboxState
+        // If single selection is enforced and multiple answers are already marked as correct, unmark all answers
+        if (checkboxState === false && activeQuestion.correctAnswers.length > 1) {
+            modifiedQuestion.correctAnswers = []
+        }
+        modifyQuestion(modifiedQuestion, questions, setQuestions)
+    }
+}
+
+export default handleCheckboxChange;
