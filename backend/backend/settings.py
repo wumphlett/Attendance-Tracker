fd5d60e6e17d9bc50aff1ff278154fe2ab54ec17
Will Humphlett <will@humphlett.net>
2023-04-10 05:13:43 -0500

    typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_cas_ng.middleware.CASMiddleware'
+    'django_cas_ng.middleware.CASMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
