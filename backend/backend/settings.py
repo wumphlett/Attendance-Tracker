4fab85f64071d37302fb4b8b1106ee6ddf6616e8
Will Humphlett <will@humphlett.net>
2023-04-10 05:17:42 -0500

    use ssl devserver

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_cas_ng',
     'rest_framework_simplejwt.token_blacklist',
-    'attendance'
+    'attendance',
+    'sslserver',
 ]
 
 REST_FRAMEWORK = {
