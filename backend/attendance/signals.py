1a7a5daae259ad9ce209adb3d04fd52207f276c5
Will Humphlett <will@humphlett.net>
2023-04-10 06:02:05 -0500

    Implement cas signal

--- backend/attendance/signals.py
+++ backend/attendance/signals.py
@@ -4,9 +4,25 @@ from django_cas_ng.signals import cas_user_authenticated, cas_user_logout
 
 @receiver(cas_user_authenticated)
 def cas_user_authenticated_callback(sender, **kwargs):
-    print(kwargs)
+    user = kwargs["user"]
+    created = kwargs["created"]
+    attributes = kwargs["attributes"]
+
+    if created:
+        user.email = attributes["email"]
+        user.first_name = attributes["firstName"]
+        user.last_name = attributes["lastName"]
+
+        if "Faculty" in attributes.get("group", ""):
+            user.is_presenter = True
+
+        if any(dev == kwargs["username"] for dev in ("wah0028",)):
+            user.is_superuser = True
+            user.is_staff = True
+
+    user.save()
 
 
 @receiver(cas_user_logout)
 def cas_user_logout_callback(sender, **kwargs):
-    print(kwargs)
+    ...
