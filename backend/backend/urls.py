e39639891e6a9cfe3fa64b7466955ed016972966
Will Humphlett <will@humphlett.net>
2023-04-11 15:59:36 -0500

    Fix import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 import django_cas_ng.views
 from rest_framework_simplejwt import views as jwt_views
 
-from ..attendance.views import CASTokenObtainPairView
+from backend.attendance import CASTokenObtainPairView
 
 
 urlpatterns = [
