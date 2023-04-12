951819b86ab1198dd0414c6fcded3ca83290d921
Will Humphlett <will@humphlett.net>
2023-04-11 20:04:46 -0500

    reinstate overwrite

--- backend/attendance/signals.py
+++ backend/attendance/signals.py
@@ -6,28 +6,24 @@ from .models import User
 
 @receiver(cas_user_authenticated)
 def cas_user_authenticated_callback(sender, **kwargs):
+    user = kwargs["user"]
+    # created = kwargs["created"]
     attributes = kwargs["attributes"]
     print(attributes)
 
-    if not User.objects.filter(username=kwargs["username"]).first():
-        user = kwargs["user"]
-        # created = kwargs["created"]
-        attributes = kwargs["attributes"]
-        print(attributes)
+    user.username = kwargs["username"]
+    user.email = attributes["email"]
+    user.first_name = attributes["firstName"]
+    user.last_name = attributes["lastName"]
 
-        user.username = kwargs["username"]
-        user.email = attributes["email"]
-        user.first_name = attributes["firstName"]
-        user.last_name = attributes["lastName"]
+    if "Faculty" in attributes.get("group", ""):
+        user.is_presenter = True
 
-        if "Faculty" in attributes.get("group", ""):
-            user.is_presenter = True
+    if any(dev == kwargs["username"] for dev in ("wah0028", "ewb0020", "qzl0037")):
+        user.is_superuser = True
+        user.is_staff = True
 
-        if any(dev == kwargs["username"] for dev in ("wah0028", "ewb0020", "qzl0037")):
-            user.is_superuser = True
-            user.is_staff = True
-
-        user.save()
+    user.save()
 
 
 @receiver(cas_user_logout)
