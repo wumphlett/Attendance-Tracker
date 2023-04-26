368ae19fa653abefc286368b705630504e2c85d0
Will Humphlett <will@humphlett.net>
2023-04-26 13:07:37 -0500

    Fix jwt interceptors

--- frontend/packages/common/src/interceptors/axios.js
+++ frontend/packages/common/src/interceptors/axios.js
@@ -17,29 +17,32 @@ const register_interceptors = (axios) => {
   axios.defaults.withCredentials = true;
   axios.defaults.headers.common['Content-Type'] = 'application/json'
 
-  let refresh = false;
-
   axios.interceptors.response.use(resp => resp, async error => {
-    // TODO Verify the refresh logic is functional
-    if (error.response.status === 401 && !refresh) {
-        refresh = true;
+    const config = error?.config;
+
+    if (error.response.status === 403 && !config?.sent) {
+      config.sent = true;
 
       const response = await axios.post('https://api.auttend.com/token/refresh/', {
-        refresh:localStorage.getItem('refresh_token')
+        refresh: localStorage.getItem('refresh_token')
       });
 
       if (response.status === 200) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${response.data['access']}`;
         localStorage.setItem('access_token', response.data.access);
         localStorage.setItem('refresh_token', response.data.refresh);
 
-        return axios(error.config);
+        config.headers = {
+          ...config.headers,
+          authorization: `Bearer ${localStorage.getItem('access_token')}`,
+        };
+        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+
+        return axios(config);
       }
     }
-    refresh = false;
 
-    // localStorage.clear();
-    // window.location.href = `https://api.auttend.com/accounts/login?next=${window.location.href}`;
+    localStorage.clear();
+    window.location.href = `https://api.auttend.com/accounts/login?next=${window.location.href}`;
     return error;
   });
 };
